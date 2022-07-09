2fc7f308957beb3d656a61d8439b54cf806f826d
GhustOne <71855034+GhustOne@users.noreply.github.com>
2022-07-09 05:33:38 +0300

    Add files via upload

---
+++ cheesemenu/ui/cheese.lua
@@ -0,0 +1,86 @@
+return {
+	["outline"] = false,
+	["height"] = 0.35,
+	["border"] = 0.0013888,
+	["header"] = [[cheese_menu]],
+	["max_features"] = 14,
+	["width"] = 0.2,
+	["feature_offset"] = 0.025,
+	["x"] = 0.73046875,
+	["y"] = 0.24305555555556,
+	["background_sprite"] = {
+		["size"] = 1,
+		["offset"] = {
+			["x"] = 0,
+			["y"] = 0,
+		},
+	},
+	["feature_scale"] = {
+		["x"] = 0.2,
+		["y"] = 0.025,
+	},
+	["footer"] = {
+		["footer_size"] = 0.019444444,
+		["draw_footer"] = true,
+		["footer_pos_related_to_background"] = false,
+		["padding"] = 0.0078125,
+		["footer_text"] = [[Made by GhostOne]],
+	},
+	["color"] = {
+		["sprite"] = 3875536895,
+		["background"] = {
+			["b"] = 0,
+			["a"] = 0,
+			["r"] = 0,
+			["g"] = 0,
+		},
+		["border"] = {
+			["b"] = 0,
+			["a"] = 180,
+			["r"] = 0,
+			["g"] = 0,
+		},
+		["notifications"] = {
+			["b"] = 0,
+			["a"] = 255,
+			["r"] = 255,
+			["g"] = 200,
+		},
+		["footer"] = {
+			["b"] = 0,
+			["a"] = 170,
+			["r"] = 255,
+			["g"] = 160,
+		},
+		["text_selected"] = {
+			["b"] = 0,
+			["a"] = 180,
+			["r"] = 255,
+			["g"] = 200,
+		},
+		["text"] = {
+			["b"] = 0,
+			["a"] = 180,
+			["r"] = 0,
+			["g"] = 0,
+		},
+		["footer_text"] = {
+			["b"] = 0,
+			["a"] = 180,
+			["r"] = 0,
+			["g"] = 0,
+		},
+		["feature"] = {
+			["b"] = 0,
+			["a"] = 170,
+			["r"] = 255,
+			["g"] = 160,
+		},
+		["feature_selected"] = {
+			["b"] = 0,
+			["a"] = 200,
+			["r"] = 0,
+			["g"] = 0,
+		},
+	},
+}
