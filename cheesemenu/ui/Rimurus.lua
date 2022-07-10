f5ab176bdf788bac4fa37b4d222d400fec9abda9
GhustOne <71855034+GhustOne@users.noreply.github.com>
2022-07-10 10:29:39 +0300

    Add files via upload

---
+++ cheesemenu/ui/Rimurus.lua
@@ -0,0 +1,86 @@
+return {
+	["feature_offset"] = 0.025,
+	["outline"] = false,
+	["y"] = 0.23055555555556,
+	["x"] = 0.362109375,
+	["width"] = 0.2,
+	["max_features"] = 13,
+	["border"] = 0.0041666666666667,
+	["height"] = 0.325,
+	["header"] = [[eclipse banner]],
+	["footer"] = {
+		["footer_size"] = 0.025694444444444,
+		["padding"] = 0.00390625,
+		["footer_text"] = [[Made by GhostOne]],
+		["footer_pos_related_to_background"] = true,
+		["draw_footer"] = true,
+	},
+	["background_sprite"] = {
+		["size"] = 0.6448362720403,
+		["offset"] = {
+			["y"] = 0,
+			["x"] = 0.0,
+		},
+	},
+	["color"] = {
+		["sprite"] = 4294967295,
+		["feature"] = {
+			["g"] = 0,
+			["r"] = 0,
+			["a"] = 150,
+			["b"] = 0,
+		},
+		["footer"] = {
+			["g"] = 0,
+			["r"] = 0,
+			["a"] = 220,
+			["b"] = 0,
+		},
+		["background"] = {
+			["g"] = 0,
+			["r"] = 0,
+			["a"] = 255,
+			["b"] = 0,
+		},
+		["border"] = {
+			["g"] = 0,
+			["r"] = 79,
+			["a"] = 255,
+			["b"] = 141,
+		},
+		["text"] = {
+			["g"] = 255,
+			["r"] = 255,
+			["a"] = 180,
+			["b"] = 255,
+		},
+		["notifications"] = {
+			["g"] = 100,
+			["r"] = 255,
+			["a"] = 220,
+			["b"] = 255,
+		},
+		["feature_selected"] = {
+			["g"] = 0,
+			["r"] = 91,
+			["a"] = 255,
+			["b"] = 164,
+		},
+		["footer_text"] = {
+			["g"] = 255,
+			["r"] = 255,
+			["a"] = 225,
+			["b"] = 255,
+		},
+		["text_selected"] = {
+			["g"] = 255,
+			["r"] = 255,
+			["a"] = 200,
+			["b"] = 255,
+		},
+	},
+	["feature_scale"] = {
+		["y"] = 0.025,
+		["x"] = 0.2,
+	},
+}
