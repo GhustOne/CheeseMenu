7cac7267ca187e17cf260eaa0feb1c318e63e02b
GhustOne <71855034+GhustOne@users.noreply.github.com>
2022-07-11 14:29:18 +0300

    Update CMAutoUpdater.lua

--- CMAutoUpdater.lua
+++ CMAutoUpdater.lua
@@ -3,13 +3,13 @@ local appdata_path = utils.get_appdata_path("PopstarDevs", "2Take1Menu")
 
 local filePaths = {
 	cheesemenu = appdata_path.."\\scripts\\",
-	gltw = appdata_path.."\\scripts\\cheesemenu\\libs\\"
-	getinput = appdata_path.."\\scripts\\cheesemenu\\libs\\"
+	gltw = appdata_path.."\\scripts\\cheesemenu\\libs\\",
+	getinput = appdata_path.."\\scripts\\cheesemenu\\libs\\",
 }
 local files = {
 	cheesemenu = [[https://raw.githubusercontent.com/GhustOne/CheeseMenu/main/cheesemenu.lua]],
 	gltw = [[https://raw.githubusercontent.com/GhustOne/CheeseMenu/main/cheesemenu/libs/GLTW.lua]],
-	getinput = [[https://raw.githubusercontent.com/GhustOne/CheeseMenu/main/cheesemenu/libs/Get%20Input.lua]]
+	getinput = [[https://raw.githubusercontent.com/GhustOne/CheeseMenu/main/cheesemenu/libs/Get%20Input.lua]],
 }
 
 for k, v in pairs(files) do
@@ -26,7 +26,7 @@ if status then
 	for k, v in pairs(files) do
 		local currentFile = io.open(filePaths[k], "w+")
 		if currentFile then
-			currentFile:write(v)
+			--currentFile:write(v)
 			currentFile:flush()
 			currentFile:close()
 		else
