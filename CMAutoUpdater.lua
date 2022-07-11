989f5d0ab027b7fc8af6444f5208cf81057520c3
GhustOne <71855034+GhustOne@users.noreply.github.com>
2022-07-11 14:20:13 +0300

    Update CMAutoUpdater.lua

--- CMAutoUpdater.lua
+++ CMAutoUpdater.lua
@@ -1,40 +1,38 @@
-return function()
-	local status = true
-	local appdata_path = utils.get_appdata_path("PopstarDevs", "2Take1Menu")
+local status = true
+local appdata_path = utils.get_appdata_path("PopstarDevs", "2Take1Menu")
 
-	local filePaths = {
-		cheesemenu = appdata_path.."\\scripts\\",
-		gltw = appdata_path.."\\scripts\\cheesemenu\\libs\\"
-		getinput = appdata_path.."\\scripts\\cheesemenu\\libs\\"
-	}
-	local files = {
-		cheesemenu = [[https://raw.githubusercontent.com/GhustOne/CheeseMenu/main/cheesemenu.lua]],
-		gltw = [[https://raw.githubusercontent.com/GhustOne/CheeseMenu/main/cheesemenu/libs/GLTW.lua]],
-		getinput = [[https://raw.githubusercontent.com/GhustOne/CheeseMenu/main/cheesemenu/libs/Get%20Input.lua]]
-	}
+local filePaths = {
+	cheesemenu = appdata_path.."\\scripts\\",
+	gltw = appdata_path.."\\scripts\\cheesemenu\\libs\\"
+	getinput = appdata_path.."\\scripts\\cheesemenu\\libs\\"
+}
+local files = {
+	cheesemenu = [[https://raw.githubusercontent.com/GhustOne/CheeseMenu/main/cheesemenu.lua]],
+	gltw = [[https://raw.githubusercontent.com/GhustOne/CheeseMenu/main/cheesemenu/libs/GLTW.lua]],
+	getinput = [[https://raw.githubusercontent.com/GhustOne/CheeseMenu/main/cheesemenu/libs/Get%20Input.lua]]
+}
 
-	for k, v in pairs(files) do
-		local responseCode, file = web.get(v)
-		if responseCode == 200 then
-			files[k] = file
-		else
-			status = false
-			break
-		end
+for k, v in pairs(files) do
+	local responseCode, file = web.get(v)
+	if responseCode == 200 then
+		files[k] = file
+	else
+		status = false
+		break
 	end
-
-	if status then
-		for k, v in pairs(files) do
-			local currentFile = io.open(filePaths[k], "w+")
-			if currentFile then
-				currentFile:write(v)
-				currentFile:flush()
-				currentFile:close()
-			else
-				status = "ERROR REPLACING"
-			end
-		end
-	end
-
-	return status
 end
+
+if status then
+	for k, v in pairs(files) do
+		local currentFile = io.open(filePaths[k], "w+")
+		if currentFile then
+			currentFile:write(v)
+			currentFile:flush()
+			currentFile:close()
+		else
+			status = "ERROR REPLACING"
+		end
+	end
+end
+
+return status
