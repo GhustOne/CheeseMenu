275d490df23fce9985723129a08aafc82d614165
GhustOne <71855034+GhustOne@users.noreply.github.com>
2022-07-29 00:02:16 +0300

    Add files via upload
    - memoize v2 ~by kektram
    - draw outline improved thanks to kektram

---
+++ cheesemenu/libs/CheeseUtilities.lua
@@ -0,0 +1,69 @@
+--Made by GhostOne
+local cheeseUtils = {}
+
+-- Credit to kektram for this whole function ~ a little modified to focus on fractionals
+cheeseUtils.memoize = {}
+do
+	local sign_bit_x <const> = 1 << 62
+	local sign_bit_y <const> = 1 << 61
+	local max_30_bit_num <const> = 1073740823
+	local v2 <const> = v2
+	local memoized <const> = {}
+	function cheeseUtils.memoize.v2(x, y)
+		x = x or 0
+		y = y or 0
+		local xi = x * 100000000 // 1 -- modified from 1000 to 100,000,000 to support up to 10^-7 fractional digits ~ this limits it to either single or double digits with fractions
+		local yi = y * 100000000 // 1 -- same here
+		if xi >= -max_30_bit_num
+		and xi <= max_30_bit_num
+		and yi >= -max_30_bit_num
+		and yi <= max_30_bit_num then
+			local signs = 0
+			if xi < 0 then
+				xi = xi * -1
+				signs = signs | sign_bit_x
+			end
+			if yi < 0 then
+				yi = yi * -1
+				signs = signs | sign_bit_y
+			end
+			local hash <const> = signs | xi << 30 | yi
+			memoized[hash] = memoized[hash] or v2(x, y)
+			return memoized[hash]
+		else
+			return v2(x, y)
+		end
+	end
+end
+--
+
+function cheeseUtils.draw_outline(v2pos, v2size, color, thickness)
+    local thickness_y = thickness / graphics.get_screen_height() * 2
+    local thickness_x = thickness / graphics.get_screen_width() * 2
+
+    scriptdraw.draw_rect(
+        cheeseUtils.memoize.v2(v2pos.x, v2pos.y - (v2size.y/2)),
+        cheeseUtils.memoize.v2(v2size.x + thickness_x, thickness_y),
+        color
+    )
+
+    scriptdraw.draw_rect(
+        cheeseUtils.memoize.v2(v2pos.x, v2pos.y + (v2size.y/2)),
+        cheeseUtils.memoize.v2(v2size.x + thickness_x, thickness_y),
+        color
+    )
+
+    scriptdraw.draw_rect(
+        cheeseUtils.memoize.v2(v2pos.x - (v2size.x/2), v2pos.y),
+        cheeseUtils.memoize.v2(thickness_x, v2size.y - thickness_y),
+        color
+    )
+
+    scriptdraw.draw_rect(
+        cheeseUtils.memoize.v2(v2pos.x + (v2size.x/2), v2pos.y),
+        cheeseUtils.memoize.v2(thickness_x, v2size.y - thickness_y),
+        color
+    )
+end
+
+return cheeseUtils
