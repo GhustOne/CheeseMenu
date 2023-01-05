f6cad506bbf28be7be753b347ece787fbb13ba6b
GhustOne <71855034+GhustOne@users.noreply.github.com>
2023-01-05 21:38:15 +0300

    Update CMAutoUpdater.lua

--- CMAutoUpdater.lua
+++ CMAutoUpdater.lua
@@ -2,42 +2,28 @@ local status = true
 local appdata_path = utils.get_appdata_path("PopstarDevs", "2Take1Menu")
 
 local filePaths = {
-	cheesemenu = appdata_path.."\\scripts\\cheesemenu.lua",
-	gltw = appdata_path.."\\scripts\\cheesemenu\\libs\\GLTW.lua",
-	getinput = appdata_path.."\\scripts\\cheesemenu\\libs\\Get Input.lua",
-	cheeseUtils = appdata_path.."\\scripts\\cheesemenu\\libs\\CheeseUtilities.lua",
-	proddysManager = appdata_path.."\\scripts\\cheesemenu\\libs\\Proddy's Script Manager.lua",
-}
-local files = {
-	cheesemenu = [[https://raw.githubusercontent.com/GhustOne/CheeseMenu/main/cheesemenu.lua]],
-	gltw = [[https://raw.githubusercontent.com/GhustOne/CheeseMenu/main/cheesemenu/libs/GLTW.lua]],
-	getinput = [[https://raw.githubusercontent.com/GhustOne/CheeseMenu/main/cheesemenu/libs/Get%20Input.lua]],
-	cheeseUtils = [[https://raw.githubusercontent.com/GhustOne/CheeseMenu/main/cheesemenu/libs/CheeseUtilities.lua]],
-	proddysManager = [[https://raw.githubusercontent.com/GhustOne/CheeseMenu/main/cheesemenu/libs/Proddy's%20Script%20Manager.lua]],
+	["cheesemenu.lua"] = appdata_path.."\\scripts\\cheesemenu.lua",
+	["GLTW.lua"] = appdata_path.."\\scripts\\cheesemenu\\libs\\GLTW.lua",
+	["Get Input.lua"] = appdata_path.."\\scripts\\cheesemenu\\libs\\Get Input.lua",
+	["CheeseUtilities.lua"] = appdata_path.."\\scripts\\cheesemenu\\libs\\CheeseUtilities.lua",
+	["Proddy's Script Manager.lua"] = appdata_path.."\\scripts\\cheesemenu\\libs\\Proddy's Script Manager.lua",
 }
 
-local all_files = 0
-local downloaded_files = 0
-for k, v in pairs(files) do
-	all_files = all_files + 1
-	menu.create_thread(function()
-		local responseCode, file = web.get(v)
-		if responseCode == 200 then
-			files[k] = file
-			downloaded_files = downloaded_files + 1
-		else
-			print("Failed to download: "..v)
-			status = false
-		end
-	end)
+local responseCode, responseBody = web.get([[https://raw.githubusercontent.com/GhustOne/CheeseMenu/dev/aioUpdate.lua]])
+if responseCode ~= 200 then
+	print("Failed to download update.")
+	status = false
 end
-while downloaded_files < all_files and status do
-	system.wait(0)
+local chunk = load(responseBody)
+if not chunk then
+	status = false
+	return status
 end
+local updatedFiles = chunk()
 
 if status then
-	for k, v in pairs(files) do
-		local currentFile = io.open(filePaths[k], "a+")
+	for _, v in pairs(filePaths) do
+		local currentFile = io.open(v, "a+")
 		if not currentFile then
 			status = "ERROR REPLACING"
 			break
@@ -45,14 +31,15 @@ if status then
         currentFile:close()
 	end
 	if status ~= "ERROR REPLACING" then
-		for k, v in pairs(files) do
-			local currentFile = io.open(filePaths[k], "w+b")
+		for k, v in pairs(filePaths) do
+			local currentFile = io.open(v, "w+b")
 			if currentFile then
-				currentFile:write(v)
+				currentFile:write(updatedFiles[k])
 				currentFile:flush()
 				currentFile:close()
 			else
 				status = "ERROR REPLACING"
+				break
 			end
 		end
 	end
