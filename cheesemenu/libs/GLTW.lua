d6428da806335e7c997b6815195cd9e445940306
GhustOne <71855034+GhustOne@users.noreply.github.com>
2023-03-06 18:42:20 +0300

    Update GLTW.lua
    some optimizations

--- cheesemenu/libs/GLTW.lua
+++ cheesemenu/libs/GLTW.lua
@@ -2,48 +2,68 @@
 -- L00naMods "Even if you say L00na is a bitch just put my name in there somewhere"
 -- Ghost's Lua Table Writer
 --[[
-nil			gltw.write(table table, string name, string path|nil, table index exclusions, skip empty tables)
--- example gltw.write({name = "l00na", iq = -1, braincells = {}}, "something", "folder1\\", {"name"}, true) < this will not write 'name' (excluded) or 'braincells' (empty)
+table[] string	gltw.write(table, string name, string path|nil, table index exclusions, skip empty tables, compile)
+example: gltw.write({name = "l00na", iq = -1, braincells = {}}, "something", "folder1\\", {"name"}, true) < this will not write 'name' (excluded) or 'braincells' (empty)
 
-table[]		gltw.read(string name, string path|nil(in same path as lua), table|nil, bool|nil)
+table[]	        gltw.read(string name, string path|nil(in same path as lua), table|nil, bool|nil, bool|nil)
 -- if a table is the 3rd arg then whatever is read from the file will be added to it without overwriting stuff that isn't in the saved file
--- if the 4th arg is true the function won't throw an error if the file doesn't exist and will return nil
+-- if the 4th arg is true it will compare types of entries in 3rd arg table and the read one, if they match or are nil it will write to 3rd arg table
+-- if the 5th arg is true the function won't throw an error if the file doesn't exist and will return nil
 ]]
 
-local gltw = {}
-local type <const> = type
-local l_next <const> = next
-local ipairs <const> = ipairs
-local long_str_levels <const> = {}
+local gltw				<const>	= {}
+local type				<const> = type
+local l_next			<const> = next
+local ipairs			<const> = ipairs
+local tostring			<const> = tostring
+local string_format		<const> = string.format
+local string_match		<const> = string.match
+
+local long_str_levels	<const> = {}
 for i = 0, 100 do
-	long_str_levels[i] = string.rep("=", i+1)
+	long_str_levels[i]			= string.rep("=", i+1)
 end
 
-function gltw.write_table(tableTW, indentation, exclusions, exclude_empty, string_lines)
+local str_level_pattern <const> = "%](=*)%]"
+
+-- Thanks to Proddy for tips on optimization
+local write_table
+function gltw.write_table(tableTW, indentation, exclusions, exclude_empty, string_lines, string_lines_count)
 	for k, v in l_next, tableTW do
 		if not exclusions[k] then
-			local typeofv = type(v)
+			local typeofv <const> = type(v)
 			local index
 			if type(k) == "number" then
 				index = "["..k.."] = "
 			else
-				index = "["..string.format("%q", k).."] = "
+				index = "["..string_format("%q", k).."] = "
 			end
 
 			if typeofv == "string" then
-				local long_str_level = v:match("%](=*)%]")
+				string_lines_count = string_lines_count + 1
+
+				local long_str_level = string_match(v, str_level_pattern)
 				long_str_level = long_str_level and long_str_levels[#long_str_level] or ""
-				string_lines[#string_lines + 1] = indentation..index.."["..long_str_level.."["..v.."]"..long_str_level.."],"
+				string_lines[string_lines_count] = indentation..index.."["..long_str_level.."["..v.."]"..long_str_level.."],"
+
 			elseif typeofv ~= "function" and typeofv ~= "table" then
-				string_lines[#string_lines + 1] = indentation..index..tostring(v)..","
+				string_lines_count = string_lines_count + 1
+				string_lines[string_lines_count] = indentation..index..tostring(v)..","
+
 			elseif typeofv == "table" and (exclude_empty and l_next(v) or not exclude_empty) then
-				string_lines[#string_lines + 1] = indentation..index.."{"
-				gltw.write_table(v, indentation.."	", exclusions, exclude_empty, string_lines)
-				string_lines[#string_lines + 1] = indentation.."},"
+				string_lines_count = string_lines_count + 1
+				string_lines[string_lines_count] = indentation..index.."{"
+				string_lines_count = write_table(v, indentation.."	", exclusions, exclude_empty, string_lines, string_lines_count)
+
+				string_lines_count = string_lines_count + 1
+				string_lines[string_lines_count] = indentation.."},"
 			end
 		end
 	end
+
+	return string_lines_count
 end
+write_table = gltw.write_table
 
 function gltw.write(tableTW, name, path, exclusions, exclude_empty, compiled)
 	local convertedExclusions = {}
@@ -60,9 +80,10 @@ function gltw.write(tableTW, name, path, exclusions, exclude_empty, compiled)
 	end
 
 	local string_lines = {}
+	local string_lines_count = 1
 
-	string_lines[#string_lines + 1] = "return {"
-	gltw.write_table(tableTW, "	", convertedExclusions, exclude_empty, string_lines)
+	string_lines[string_lines_count] = "return {"
+	gltw.write_table(tableTW, "	", convertedExclusions, exclude_empty, string_lines, string_lines_count)
 	string_lines[#string_lines + 1] = "}"
 
 	if name then
@@ -99,17 +120,17 @@ function gltw.add_to_table(getTable, addToTable, typeMatched)
 end
 
 function gltw.read(name, path, addToTable, typeMatched, overrideError)
-	if overrideError and not utils.file_exists(path..name..".lua") then
+	name = string_match(name, "%.%w+") and name or name..".lua"
+	if overrideError and not utils.file_exists(path..name) then
 		return
 	end
 
 	path = path or ""
-	--[[ if type(tableRT) == "string" then
-		name, path = tableRT, name or path
-		tableRT = nil
-	end ]]
+	if not (path:sub(-1) == "\\" or path:sub(-1) == "/") then
+		path = path .. "\\"
+	end
 
-	local readTable = loadfile(path..name..".lua", "tb")()
+	local readTable = loadfile(path..name, "tb")()
 	if addToTable then
 		gltw.add_to_table(readTable, addToTable, typeMatched)
 	end
