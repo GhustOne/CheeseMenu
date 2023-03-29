ddbf38fb739a03b56c86b1777bfd2b006a484821
GhustOne <71855034+GhustOne@users.noreply.github.com>
2023-03-29 23:46:44 +0300

    Update GLTW.lua
    - Added gltw.read_fmap
    - Added gltw.write_fmap
    - Using _loadfile now
    - Handling relative paths to convert to absolute
    - Better error message when file is not found
    - Fixed double .lua bug

--- cheesemenu/libs/GLTW.lua
+++ cheesemenu/libs/GLTW.lua
@@ -19,6 +19,9 @@ local tostring			<const> = tostring
 local string_format		<const> = string.format
 local string_match		<const> = string.match
 
+local appdata_path		<const> = utils and utils.get_appdata_path("PopstarDevs", "2Take1Menu").."\\" or ""
+local _loadfile			<const> = _loadfile or loadfile
+
 local long_str_levels	<const> = {}
 for i = 0, 100 do
 	long_str_levels[i]			= string.rep("=", i+1)
@@ -75,6 +78,7 @@ function gltw.write(tableTW, name, path, exclusions, exclude_empty, compiled)
 	assert(tableTW, "no table was provided"..(name and " to write for file '"..name.."'" or ""))
 
 	if name then
+		name = name:sub(-4) == ".lua" and name or name..".lua"
 		path = path or ""
 		assert(type(name) == "string" and type(path) == "string", "name or path isn't a string")
 	end
@@ -87,7 +91,7 @@ function gltw.write(tableTW, name, path, exclusions, exclude_empty, compiled)
 	string_lines[#string_lines + 1] = "}"
 
 	if name then
-		local file = io.open(path..name..".lua", "wb")
+		local file = io.open(path..name, "wb")
 		assert(file, "'"..name.."' was not created.")
 
 		local stringified = table.concat(string_lines, "\n")
@@ -120,7 +124,7 @@ function gltw.add_to_table(getTable, addToTable, typeMatched)
 end
 
 function gltw.read(name, path, addToTable, typeMatched, overrideError)
-	name = string_match(name, "%.%w+") and name or name..".lua"
+	name = name:sub(-4) == ".lua" and name or name..".lua"
 	if overrideError and not utils.file_exists(path..name) then
 		return
 	end
@@ -129,12 +133,78 @@ function gltw.read(name, path, addToTable, typeMatched, overrideError)
 	if not (path:sub(-1) == "\\" or path:sub(-1) == "/") then
 		path = path .. "\\"
 	end
+	if not path:find("C:") then
+		path = appdata_path..path
+	end
 
-	local readTable = loadfile(path..name, "tb")()
+	local readTable = _loadfile(path..name, "tb")
+	if not readTable then
+		error("File does not exist. use an absoulte path or a relative path from 2Take1Menu folder.\nFile: "..path..name)
+	end
+	readTable = readTable()
+
 	if addToTable then
 		gltw.add_to_table(readTable, addToTable, typeMatched)
 	end
+
 	return readTable
+end
+
+if menu then
+	local default_properties <const> = {"on", "value"}
+
+	local property_types <const> = {
+		on = 1,
+		value = 2,
+
+		name = -1,
+		hidden = -1,
+		data = -1,
+		hint = -1,
+
+		min = 1 << 2 | 1 << 3 | 1 << 7,
+		max = 1 << 2 | 1 << 3 | 1 << 7,
+		mod = 1 << 2 | 1 << 3 | 1 << 7,
+		str_data = 1 << 5,
+	}
+
+	---@param fmap table table containing key value pairs, where value is the feature
+	---@param name string file containing saved data including or excluding .lua
+	---@param path string absolute or relative (starts at 2Take1Menu) path of folder containing saved file
+	---@param properties table|nil a table containing properties you want to save, ie `{"on", "value", "max"}`, if nil then it'll save `on` and `value`
+	---@param compile bool|nil compile saved file
+	function gltw.write_fmap(fmap, name, path, properties, compile)
+		local tbl <const> = {}
+		for k, feat in pairs(fmap) do
+			local f <const> = {}
+			for _, property in pairs(properties or default_properties) do
+				if feat.type & property_types[property] ~= 0 then
+					f[property] = feat[property]
+				end
+			end
+			tbl[k] = next(f) and f or nil
+		end
+
+		gltw.write(tbl, name, path, nil, nil, compile)
+	end
+
+	function gltw.read_fmap(fmap, name, path)
+		local tbl <const> = gltw.read(name, path)
+		if not tbl then
+			return
+		end
+
+		for k, saved_data in pairs(tbl) do
+			local feat <const> = fmap[k]
+			if feat then
+				for property, value in pairs(saved_data) do
+					if feat.type & property_types[property] ~= 0 then
+						feat[property] = value
+					end
+				end
+			end
+		end
+	end
 end
 
 return gltw
